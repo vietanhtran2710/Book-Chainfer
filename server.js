b0a31ba7609ecbb5f9e761cbd63d92af8b330671
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-07-01 10:32:52 +0700

    init backend server

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+global.__basedir = __dirname;
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:4200",
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = require("./models");
+db.sequelize.sync({force: true}).then(() => {
+  console.log("Drop and re-sync db.");
+});
+
+app.get("/", (req, res) => {
+    res.json({ message: "Server is running" });
+});
+
+require("./routes/user.routes")(app);
+require("./routes/book.routes")(app);
+
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
