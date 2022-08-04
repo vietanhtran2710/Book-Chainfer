0c4d1581af75592f189c6d044ab8e04576eebb56
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-08-04 14:48:07 +0700

    register and login on backend

---
+++ models/index.js
@@ -0,0 +1,29 @@
+const dbConfig = require("../config/db.config.js");
+const { Sequelize, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle
+  },
+  timezone: '+07:00'
+});
+
+users = require("./user.model")(sequelize, DataTypes)
+// books = require("./book.model")(sequelize, DataTypes)
+
+// users.hasMany(books)
+
+const db = {
+  users,
+//   books
+};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+module.exports = db;
