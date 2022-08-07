31abc731da2c27bbd6dadeac10d03c0ea3803a3d
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-08-07 15:02:06 +0700

    add logging out and sessions integration

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) {
+    if (Object.keys(this.authService.currentUserValue).length === 0) {
+      window.location.replace('')
+    }
+   }
 
   ngOnInit(): void {
+  }
+
+  logOut() {
+    this.authService.logout();
   }
 
 }
