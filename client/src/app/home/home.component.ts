70eebac16c4b6d418acc6a1d8c681ac3477980a0
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-08-31 11:14:05 +0700

    create book ownership token on blockchain

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import Swal from 'sweetalert2';
 import { AuthService } from '../services/auth.service';
 import { BookService } from '../services/book.service';
+import { BlockchainService } from '../services/blockchain.service';
 
 @Component({
   selector: 'app-home',
@@ -16,6 +17,7 @@ export class HomeComponent implements OnInit {
 
   constructor(private authService: AuthService,
               private bookService: BookService,
+              private blockchainService: BlockchainService,
               private fb: FormBuilder) {
     if (Object.keys(this.authService.currentUserValue).length === 0) {
       window.location.replace('')
@@ -31,10 +33,6 @@ export class HomeComponent implements OnInit {
     this.userAddress = this.authService.currentUserValue.address
   }
 
-  logOut() {
-    this.authService.logout();
-  }
-
   fileChosen(event: any) {
     if (event.target.value) {
       this.file = <File>event.target.files[0];
@@ -47,18 +45,22 @@ export class HomeComponent implements OnInit {
       formData.append('file', this.file, this.file.name);
       formData.append('name', this.bookModel.get('name')!.value);
       formData.append('author', this.bookModel.get('author')!.value);
-      formData.append('id', '1');
-      this.bookService.uploadBook(formData).subscribe(
-        (result: any) => {
-          if (result.message == "Success") {
-            Swal.fire({
-              icon: 'success',
-              title: 'Done',
-              text: 'Book created successfully'
-            })
+      this.blockchainService.createBook(formData.get('name')!.toString(), this.authService.currentUserValue.address)
+      .then((result: any) => {
+        console.log(result);
+        formData.append('id', result.events.Transfer.returnValues.tokenId);
+        this.bookService.uploadBook(formData).subscribe(
+          (result: any) => {
+            if (result.message == "Success") {
+              Swal.fire({
+                icon: 'success',
+                title: 'Done',
+                text: 'Book created successfully'
+              })
+            }
           }
-        }
-      )
+        )
+      })
     }
   }
 }
