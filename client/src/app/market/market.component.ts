aa5327371e6fa95f0c5684ab4f631386f91442a0
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-10-23 15:19:22 +0700

    display selling tokens on client

--- client/src/app/market/market.component.ts
+++ client/src/app/market/market.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { BlockchainService } from '../services/blockchain.service';
 import { TokenService } from '../services/token.service';
 import { AuthService } from '../services/auth.service';
 import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-market',
@@ -16,32 +18,31 @@ export class MarketComponent implements OnInit {
   userAddress: string = '';
   userSellingTokens: Array<any> = [];
   otherSellingTokens: Array<any> = [];
+  marketModel: FormGroup
 
   constructor(private blockchainService: BlockchainService,
               private authService: AuthService,
-              private tokenService: TokenService) { 
+              private tokenService: TokenService,
+              private fb: FormBuilder) { 
     if (Object.keys(this.authService.currentUserValue).length === 0) {
       window.location.replace('')
     }
     else {
-      let that = this;
       this.authService.verifyToken().pipe(catchError(err => {
         window.location.replace('')
         return throwError(err);
       })).subscribe((data: any) => {
         this.userAddress = data.address;
-        let that = this;
         this.blockchainService.getPageBalance(this.userAddress)
         .then((result: any) => {
-          that.balance = result;
+          this.balance = result;
         })
         this.tokenService.getAll().subscribe(
           (result: any) => {
             this.blockchainService.getSellingToken(result)
             .then((tokens: any) => {
-              console.log(tokens);
               for (let item of tokens) {
-                if (tokens.owner == this.userAddress) {
+                if (item.owner.toLowerCase() == this.userAddress.toLowerCase()) {
                   this.userSellingTokens.push(item);
                 }
                 else {
@@ -53,9 +54,64 @@ export class MarketComponent implements OnInit {
         )
       })
     }
+    this.marketModel = this.fb.group({
+      tokenID: '',
+      bookTitle: '',
+      marketPrice: '',
+    })
   }
 
   ngOnInit(): void {
+  }
+
+  updateMarketModalInfo(bookInfo: any) {
+    console.log(bookInfo);
+    this.marketModel.setValue({
+      bookTitle: bookInfo.book.name, 
+      tokenID: bookInfo.id,
+      marketPrice: bookInfo.price
+    })
+  }
+
+  updateMarketInfo() {
+    let tokenID = this.marketModel.get('tokenID')?.value;
+    let newPrice = this.marketModel.get('marketPrice')?.value;
+    this.blockchainService.setTokenPrice(
+      tokenID, newPrice, this.userAddress
+    )
+    .then((result) => {
+      if (newPrice != 0) {
+        this.tokenService.createToken({
+          tokenId: tokenID,
+          bookId: tokenID
+        }).subscribe((result) => {
+          console.log(result);
+          Swal.fire({
+            icon: 'success',
+            title: 'Done',
+            text: `Market infomation updated successfully`
+          })
+        })
+      }
+      else {
+        this.tokenService.delete(tokenID).subscribe(
+          (result) => {
+            Swal.fire({
+              icon: 'success',
+              title: 'Done',
+              text: `Market infomation updated successfully`
+            })
+          }
+        )
+      }
+    })
+    .catch((err) => {
+
+    })
+  }
+
+  buyBook(token: any) {
+
   }
 
 }
